6d434e2b9daac0d8b8c53a499f3734b4402f9055
Burhanuddin Rashid <burhanuddinr@sahajsoft.com>
2020-11-12 16:57:43 +0530

    Added local properties

--- update_properties.sh
+++ update_properties.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 # Assign the filename
-touch local.properties
-echo "BASE_URL=\"$1\"" >> local.properties
-echo "MAP_KEY=\"$2\"" >> local.properties
-echo "RETRY_ATTEMPTS=$3" >> local.properties
-echo "THRESHOLD_VALUE=$4" >> local.properties
+
+filename="local.properties"
+if [ -e "$filename" ]; then
+  sed -i "s,.*BASE_URL.*,BASE_URL=\"$1\"," $filename
+  sed -i "s/.*MAP_KEY.*/MAP_KEY=\"$2\"/" $filename
+  sed -i "s/.*RETRY_ATTEMPTS.*/RETRY_ATTEMPTS=$3/" $filename
+  sed -i "s/.*THRESHOLD_VALUE.*/THRESHOLD_VALUE=$4/" $filename
+else
+  touch local.properties
+  echo "BASE_URL=\"$1\"" >> local.properties
+  echo "MAP_KEY=\"$2\"" >> local.properties
+  echo "RETRY_ATTEMPTS=$3" >> local.properties
+  echo "THRESHOLD_VALUE=$4" >> local.properties
+fi
